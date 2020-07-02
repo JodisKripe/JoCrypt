8a1458224614addec657efcc24e97847f93110a4
Siddharth Johri <34266009+SidJo15@users.noreply.github.com>
2020-07-02 18:55:57 +0530

    Update Database.py

--- Database.py
+++ Database.py
@@ -1,11 +1,13 @@
 #Database
 import os
+import csv
+import pandas as pd
 import mysql.connector
 def MakeDBandTbl():
 	os.system("cls")	
 	global username
 	global pswd
-	username,pswd=input("Enter username(By Default \'root\'')."),input("Enter password for mySQL database.")
+	username,pswd=input("Enter username(By Default \'root\''):"),input("Enter password for mySQL database:")
 	entry=input("Is the database and the table already created?(Y/N)\n")
 	if(entry=='N'):	
 		mydb=mysql.connector.connect(host="localhost", user=username, password=pswd)
@@ -24,7 +26,6 @@ def MakeDBandTbl():
 
 
 
-
 def AddRecord(user,method,encryption,time):
 	
 	mydb=mysql.connector.connect(host="localhost", user=username, password=pswd, database="joc_logs")
@@ -33,4 +34,25 @@ def AddRecord(user,method,encryption,time):
 	myc.execute("INSERT INTO Records VALUES (\""+user+"\",\""+method+"\",\""+encryption+"\",\""+time+"\")")
 	mydb.commit()
 	mydb.close()
+	with open("JoC_logs.csv" ,"a" , newline='') as file:
+		writer = csv.writer(file)
+		writer.writerow([user,method,encryption,time])
+	file.close()
 	#print("Done")
+
+
+def Read():                                                         
+	print("8 8888         ,o888888o.         ,o888888o.      d888888o.   ")
+	print("8 8888      . 8888     `88.      8888     `88.  .`8888:' `88. ")
+	print("8 8888     ,8 8888       `8b  ,8 8888       `8. 8.`8888.   Y8 ")
+	print("8 8888     88 8888        `8b 88 8888           `8.`8888.     ")
+	print("8 8888     88 8888         88 88 8888            `8.`8888.    ")
+	print("8 8888     88 8888         88 88 8888             `8.`8888.   ")
+	print("8 8888     88 8888        ,8P 88 8888   8888888    `8.`8888.  ")
+	print("8 8888     `8 8888       ,8P  `8 8888       .8'8b   `8.`8888. ")
+	print("8 8888      ` 8888     ,88'      8888     ,88' `8b.  ;8.`8888 ")
+	print("8 888888888888 `8888888P'         `8888888P'    `Y8888P ,88P' ")
+	print("\n\nLogs:\n")
+	data=pd.read_csv("JoC_logs.csv")
+	print(data)
+	a=input()
