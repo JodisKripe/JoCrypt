3ff8692ab61252529f6555fbfab977b152368ddf
PyromancerBoom <priyanshmagic@gmail.com>
2021-10-24 13:12:09 +0530

    pip packages installer

--- installPackages.py
+++ installPackages.py
@@ -8,6 +8,7 @@ print("Updating pip...")
 subprocess.check_call([sys.executable, '-m', 'pip', 'install','-U', 'pip'])
 
 print("\nInstalling packages...\n")
+
 for i in packagesToInstall:
 	subprocess.run([sys.executable, '-m', 'pip', 'install', i])
 	print(i,"is ready to use!")
